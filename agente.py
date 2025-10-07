d36b56a7779cbaeb6f9c0f1ce99a1cf93954d4fa
seba-peitiado <speitiado@auronix.com>
2025-10-07 19:54:30 +0000

    creating free chat agent

--- agente.py
+++ agente.py
@@ -121,8 +121,10 @@ graph_builder = StateGraph(State)
 def chatbot(state: State):
     return {"messages": [llm.invoke(state["messages"])]}
 
-graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_edge(START, "chatbot")
+graph_builder.add_node("classifier", clasify_message)
+graph_builder.add_node("router", router)
+#graph_builder.add_node("chatbot", chatbot)
+#graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 graph = graph_builder.compile()
 
