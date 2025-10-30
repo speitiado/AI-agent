c396d28fb618407c0ae7e34118d1bfbb5c8ff7c5
seba-peitiado <speitiado@auronix.com>
2025-10-30 19:51:42 +0000

    preparando la estructura del agente

--- agente.py
+++ agente.py
@@ -4,6 +4,7 @@ from langchain.tools import tool
 from langchain.chat_models import init_chat_model
 from langchain.messages import AnyMessage
 from typing_extensions import TypedDict, Annotated
+from langchain.messages import SystemMessage
 import os
 load_dotenv()
 
@@ -16,59 +17,24 @@ class MessagesState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
     llm_calls: int
 
-# def consultar_gpt(mensaje, historial):
-#     messages = historial + [{"role": "user", "content": mensaje}]
-#     response = client.chat.completions.create(
-#         model="gpt-4o-mini",  # rápido y económico
-#         messages=messages
-#     )
-#     return response.choices[0].message.content
 
-# Acciones simuladas
-def crear_reunion(mensaje):
-    return "📅 Reunión creada: mañana a las 10:00 (simulada)."
 
-def enviar_notificacion(mensaje):
-    return "🔔 Notificación enviada (simulada)."
+def node_solucion_modem(messages: MessagesState):
+    pass
 
-def procesar_pago(mensaje):
-    return "💳 Pago procesado con éxito (simulado)."
+def node_consultar_planes(messages: MessagesState):
+    pass
 
-acciones = {
-    "reunion": crear_reunion,
-    "notificacion": enviar_notificacion,
-    "pago": procesar_pago
-}
+def node_procesar_pago(messages: MessagesState):
+    pass
 
-def detectar_accion(mensaje):
-    mensaje_l = mensaje.lower()
-    if "reunion" in mensaje_l or "agendar" in mensaje_l:
-        return "reunion"
-    if "notificar" in mensaje_l or "avísame" in mensaje_l:
-        return "notificacion"
-    if "pago" in mensaje_l or "cobrar" in mensaje_l:
-        return "pago"
-    return "conversar"
+
+def router(messages: MessagesState):
+    mensaje_l = messages.lower()
+    
 
 def main():
-    historial = []
-    print("🤖 Agente AI listo (escribí 'salir' para terminar)\n")
-
-    while True:
-        user_input = input("Tú: ")
-        if user_input.lower() == "salir":
-            break
-
-        accion = detectar_accion(user_input)
-
-        if accion == "conversar":
-            respuesta = consultar_gpt(user_input, historial)
-            historial.append({"role": "user", "content": user_input})
-            historial.append({"role": "assistant", "content": respuesta})
-        else:
-            respuesta = acciones[accion](user_input)
-
-        print("Agente:", respuesta, "\n")
+  pass
 
 if __name__ == "__main__":
     main()
